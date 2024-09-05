7f2477cf321d30661fc9e0fba85e0d55778af3ba
FaizSilmi <faizsilmi45@gmail.com>
2024-09-05 09:27:25 +0700

    TUGAS2A.c

--- Algoritma Pemograman/Jobsheet 2/Evaluasi/TUGAS2A.c
+++ Algoritma Pemograman/Jobsheet 2/Evaluasi/TUGAS2A.c
@@ -1,5 +1,5 @@
 /*  
-	Nama File      : TUGAS2Ac.c 
+	Nama File      : TUGAS2A.c 
 	Programmer     : Muhammad Faiz Silmi (24343099)
 	Tgl. pembuatan : 02 September 2024 
 	Deskripsi      : program ini menghitung tahanan, arus, dan tegangan rangkaian paralel
@@ -14,18 +14,20 @@ int main(){
 	printf("Progammer : Muhammad Faiz Silmi A \n");
 	printf("NIM       : 24343099 \n");
 	printf("===================== \n");
-	float r1, r2, rt, v, i, vr1, vr2;
+	float r1, r2, r3, rt, v, i, vr1, vr2;
 	// Masukkan variabel
 	printf("Masukkan R1= ");
 	scanf("%f", &r1);
 	printf("Masukkan R2= ");
 	scanf("%f", &r2);
+	printf("Masukkan R3= ");
+	scanf("%f", &r3);
 	printf("Masukkan VR1= ");
 	scanf("%f", &vr1);
 	printf("Masukkan VR2= ");
 	scanf("%f", &vr2);
 	// Proses menghitung
-	rt = (r1 * r2) / (r1 + r2);
+	rt = 1.00 / ((1.00 / r1) + (1.00 / r2) + (1.00 / r3));
 	v = vr1 + vr2;
 	i = v / rt;
 	// Hasil

6eb6cf0c70cae1d1ba8de959508d75a068622c20
FaizSilmi <faizsilmi45@gmail.com>
2024-09-07 18:10:56 +0700

    Tugas14.c

--- Algoritma Pemograman/Tugas/Tugas14.c
+++ Algoritma Pemograman/Tugas/Tugas14.c
@@ -1,9 +1,21 @@
+/*  
+	Nama File      : Tugas14.c 
+	Programmer     : Muhammad Faiz Silmi (24343099)
+	Tgl. pembuatan : 07 September 2024 
+	Deskripsi      : program ini untuk mencari ketinggian tanah di dalam suatu bidang datar
+*/
+
 #include<stdio.h>
 #include<math.h>
 #define PHI 3.14
 
 int main(){
 	float d, k, t, p, l;
+	printf("Project Tugas 14 \n");
+	printf("===================== \n");
+	printf("Progammer : Muhammad Faiz Silmi \n");
+	printf("NIM       : 24343099 \n");
+	printf("===================== \n");
 	// Masukkan variabel
 	printf("Masukkan kedalaman= ");
 	scanf("%f", &k);

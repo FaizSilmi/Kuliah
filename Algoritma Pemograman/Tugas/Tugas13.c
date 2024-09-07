b628456051a1999081e60e44938f9677d18cbb99
FaizSilmi <faizsilmi45@gmail.com>
2024-09-07 18:09:50 +0700

    Tugas13.c

--- Algoritma Pemograman/Tugas/Tugas13.c
+++ Algoritma Pemograman/Tugas/Tugas13.c
@@ -1,12 +1,24 @@
+/*  
+	Nama File      : Tugas13.c 
+	Programmer     : Muhammad Faiz Silmi (24343099)
+	Tgl. pembuatan : 07 September 2024 
+	Deskripsi      : program ini untuk mencari selisih waktu
+*/
+
 #include<stdio.h>
 #include<stdlib.h>
 
 int main(){
+	
 	int jaw, maw, daw, totaw;
 	int jak, mak, dak, totak;
 	int totas, jas, mas, das;
 	char ch;
-	
+	printf("Project Tugas 13 \n");
+	printf("===================== \n");
+	printf("Progammer : Muhammad Faiz Silmi \n");
+	printf("NIM       : 24343099 \n");
+	printf("===================== \n");
 	printf("Masukkan waktu awal (ex: 07:30:15)= ");
 	scanf("%d%c%d%c%d", &jaw, &ch, &maw, &ch, &daw);
 	printf("Ketikkan waktu akhir (ex: 07:30:15): ");

5df599833057b921bb92b2d6d0e8d3bfc23b7937
FaizSilmi <faizsilmi45@gmail.com>
2024-09-16 15:21:23 +0700

    latihan3D.c

--- Algoritma Pemograman/Praktikum 3/latihan3D.c
+++ Algoritma Pemograman/Praktikum 3/latihan3D.c
@@ -1,6 +1,6 @@
 /* 	Nama File		: latihan3D.c
 	Programmer		: tuliskan nama dan nim anda
-	Tgl. pembuatan	: tuliskan tanggal hari praktikum anda
+	Tgl. pembuatan		: 10 September 2024
 	Deskripsi		: program ini melakukan perhitungan diskon	                            
 */
 
